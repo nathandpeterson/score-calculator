1aae5259b5b9c2e0b54daba8be2e98a0b62f369f
Nathan Daniel Peterson <natperson@gmail.com>
2017-10-13 09:30:19 -0700

    complete challenge

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 'use strict'
+const fs = require('fs')
+const path = './scores.txt'
+
+const scoreText = fs.readFileSync(path, 'utf-8')
+const scoreArray = scoreText.split('\n')
+const totals = {}
+let winner = ''
+let winningScore = 0
+scoreArray.forEach(function(e){
+  e = e.split(',')
+  let total = 0
+  for(let i = 1; i < e.length; i++){
+    total += Number(e[i])
+  }
+  totals[e[0]] = total
+  if(total > winningScore){
+    winner = e[0]
+    winningScore = total
+  }
+})
+console.log(winner)
